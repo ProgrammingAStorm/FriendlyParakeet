dd2ac51f756f48bfce61e32aaea92875431e7d70
Mark Pavel <markpavel02@gmail.com>
2022-04-09 18:07:16 -0400

    Added Prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,77 @@
-// Assignment code here
+function lengthPrompt() {
+  var length = window.prompt("Please pick a length between 8 and 128.");
+
+  if(length === "" || length === null) {
+    window.alert("The size has defualted to 8.")
+    return 8;
+  }
+  
+  length = parseInt(length);
+  
+  if (length < 8) {
+    return lengthPrompt();
+  } else if(length > 128) {
+    return lengthPrompt();
+  }
+
+  return length;
+}
+
+function charPrompt() {
+  var selections = [
+    window.confirm("Do you want lowercase characters?"),
+    window.confirm("Do you want uppercase characters?"),
+    window.confirm("Do you want numeric characters?"),
+    window.confirm("Do you want special characters?")
+  ];
+
+  var falseCount = 0;
+  for (var x = 0; x < selections.length; x++) {
+    if(!selections[x]) {
+      falseCount++
+    }
+  }
+  if (falseCount === 4) {
+    window.alert("Please select at least one character type.")
+    return charPrompt();
+  }
+
+  return selections;
+}
+
+function writePassword() {
+  var password = generatePassword(
+    lengthPrompt(),
+    charPrompt()
+  );
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword(length, selections) {
+  var charTypes = []
+  if(selections[0]) {
+    charTypes.push("low")
+  }
+  if(selections[1]) {
+    charTypes.push("up")
+  }
+  if(selections[2]) {
+    charTypes.push("num")
+  }
+  if(selections[3]) {
+    charTypes.push("spec")
+  }
+
+  
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
